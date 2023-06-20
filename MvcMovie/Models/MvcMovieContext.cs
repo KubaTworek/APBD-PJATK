34652842d52e490fdad84afac81f30604c26777a
Jakub Tworek <kubatvorek@gmail.com>
2023-06-20 16:01:04 +0200

    craete MVC Project

---
+++ MvcMovie/Models/MvcMovieContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MvcMovieContext()
+        {
+        }
+
+        public MvcMovieContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s25646;Integrated Security=True;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class SeedData
+    {
+        private readonly MvcMovieContext _context;
+
+        public SeedData(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+                if (_context.Movies.Any())
+                {
+                    return;
+                }
+
+                _context.Movies.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "A",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "B",
+                        Price = 3.99M
+                    }
+                );
+                _context.SaveChanges();
+           
+        }
+    }
+}
